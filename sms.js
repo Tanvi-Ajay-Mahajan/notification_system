d8d33480e1202af68edaa38c12d4bbd7a56b1654
Tanvi-Ajay-Mahajan <mtanvi598@gmail.com>
2025-01-23 16:40:25 +0530

    Update sms.js

--- sms.js
+++ sms.js
@@ -1,17 +1,15 @@
 const twilio = require('twilio');
 
-// Twilio configuration
-const accountSid = 'your-account-sid';
-const authToken = 'your-auth-token';
+const accountSid = 'your-twilio-sid';
+const authToken = 'your-twilio-auth-token';
 const client = twilio(accountSid, authToken);
 
-// Function to send SMS
-const sendSMS = (to, message) => {
-    return client.messages.create({
-        body: message,
-        from: '+1234567890', // Your Twilio phone number
-        to
-    });
-};
-
-module.exports = sendSMS;
+client.messages.create({
+    body: 'This is a test SMS',
+    from: '+1234567890',
+    to: '+recipient-phone-number'
+}).then((message) => {
+    console.log('SMS sent successfully:', message.sid);
+}).catch((error) => {
+    console.error('Error sending SMS:', error);
+});
